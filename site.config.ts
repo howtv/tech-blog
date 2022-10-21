d289593fb416b81e61823e18f844a1a1599a1163
Doarakko <yuki.tanaami@howtelevision.jp>
2022-10-21 18:57:56 +0900

    new ogp

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export const config = {
   },
   siteRoot:
     process.env.NODE_ENV === "production"
-      ? "https://team-blog-hub.vercel.app"
+      ? "https://tech.howtelevision.co.jp"
       : "http://localhost:3000",
   headerLinks: [
     {
